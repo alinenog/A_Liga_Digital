664d6a98c87db3fb0cd7248b93105359f12694f4
Aline Nogueira <61569421+alinenog@users.noreply.github.com>
2022-04-30 23:21:19 -0300

    Add exerc

--- JavaScript/Ex014_for.js
+++ JavaScript/Ex014_for.js
@@ -8,6 +8,22 @@ notas.push(10)
 notas.push(8)
 notas.push(7.8)
 
+console.log()
 for (let index = 0; index < notas.length; index++) {
     console.log('Indice: ' + index + ' Nota -> ' + notas[index])
 }
+
+//-------------------------------------------------------
+//Estrutura do For
+console.log()
+let tamanhoMaximo = 5;
+
+for (let contador = 0; contador < tamanhoMaximo; contador++) {
+    console.log(contador);
+}
+
+//-------------------------------------------------------
+
+
+
+
---
+++ JavaScript/Ex18_Json.js
@@ -0,0 +1,40 @@
+//Aula 17
+
+/*Não pode ter funções;
+Não pode ter comentários;
+Todo texto sempre tem aspas duplas;
+As propriedades sempre tem aspas duplas.*/
+
+//https://rickandmortyapi.com/ 
+
+
+//Modelo I
+{"id":359,
+"name":"Tortured Morty",
+"status":"unknown",
+"species":"Human",
+"type":"",
+"gender":"Male",
+"origin":{"name":"unknown","url":""},
+"location":{"name":"Citadel of Ricks",
+"url":"https://rickandmortyapi.com/api/location/3"},
+"image":"https://rickandmortyapi.com/api/character/avatar/359.jpeg",
+"episode":["https://rickandmortyapi.com/api/episode/10"],
+"url":"https://rickandmortyapi.com/api/character/359",
+"created":"2018-01-10T18:14:50.992Z"}
+
+//Modelo II
+{ "id": 16, 
+"name": "St. Gloopy Noops Hospital", 
+"type": "Space station", "dimension": "unknown",
+"residents": ["https://rickandmortyapi.com/api/character/105", 
+"https://rickandmortyapi.com/api/character/263",
+"https://rickandmortyapi.com/api/character/321", 
+"https://rickandmortyapi.com/api/character/383", 
+"https://rickandmortyapi.com/api/character/384", 
+"https://rickandmortyapi.com/api/character/454", 
+"https://rickandmortyapi.com/api/character/455", 
+"https://rickandmortyapi.com/api/character/456", 
+"https://rickandmortyapi.com/api/character/460"], 
+"url":"https://rickandmortyapi.com/api/location/16", 
+"created": "2017-11-18T11:43:20.075Z" }
