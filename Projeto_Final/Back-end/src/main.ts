e24e881a3d9814c9fcbc279be0c439bec79a7a15
Aline Nogueira <61569421+alinenog@users.noreply.github.com>
2022-05-10 21:05:25 -0300

    Update

--- Projeto_Final/Back-end/src/main.ts
+++ Projeto_Final/Back-end/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Shopping list API')
     .setDescription('My shopping list API description')
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
